ebbf67648726b0e304f38e867d12558a7b7cb3c5
Karthik Nair <karthikreads@gmail.com>
2017-03-17 23:18:56 -0700

    third Commit

--- printFromRemote.py
+++ printFromRemote.py
@@ -1,33 +1,53 @@
-from github import Github
+import git,os
+import shutil
 import sys
+import yaml
+from os.path import isfile
+from git import Repo
 from flask import Flask
+
 
 app = Flask(__name__)
 
+DIR_NAME = "./config_repo73"
+REMOTE_URL = str(sys.argv[1])
+
+def pullgit_repo():
+ if (os.path.isdir(DIR_NAME)):
+  shutil.rmtree(DIR_NAME)
+ os.mkdir(DIR_NAME)
+ repo = git.Repo.init(DIR_NAME)
+ origin = repo.create_remote('origin',REMOTE_URL)
+ origin.fetch()
+ origin.pull(origin.refs[0].remote_head)
+ print "Showing files present in github repository:"
+ print "Please browse the file which you want to display contents:"
+ dirs = os.listdir(DIR_NAME)
+ for file in dirs:
+  print file
+ print "---- END ----"
+
 
 @app.route("/v1/<filename>")
-def printfrmremoteresource(filename):
-    cmdlineargs = (sys.argv[1]).split("/")
-    userID = cmdlineargs[3]
-    repository = cmdlineargs[4]
-    repo = Github().get_user(userID).get_repo(repository)
-    extension = '.json', '.yml'
-    out = []
-    if (filename != None):
-        if filename.endswith(extension):
-            configfile = repo.get_contents(filename)
-            out.append(configfile.decoded_content)
-            return "<br/>".join(out)
-        else:
-            return "Incorrect file extension!"
-    else:
-        return "The requested url is unavailable, Page not found"
-
-
-@app.route("/")
-def hello():
-    return "Hello from Dockerized Flask App!! "
+def displayfilecontent(filename):
+ extensions = '.yml','json'
+ if filename.endswith(extensions):
+   filepath= DIR_NAME +"/"+ str(filename)
+   if isfile(filepath):
+    with open(filepath, "r") as stream:
+     docs = yaml.load_all(stream)
+     for doc in docs:
+      output=[]
+      for k,v in doc.items():
+        output.append(v)
+      return "<br/>".join(output)
+    stream.close()
+   else:
+    return "Page not found"
+ else:
+   return "file cannot open! No access rights"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    pullgit_repo()
+    app.run(debug=True,host='0.0.0.0')
